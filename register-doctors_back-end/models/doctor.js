2739dfb23ac40ac72648ededd08a40e50fb77e0a
Ed Santos <edsantos2822@gmail.com>
2022-04-09 18:20:44 -0300

    style: adicionando comentarios

--- register-doctors_back-end/models/doctor.js
+++ register-doctors_back-end/models/doctor.js
@@ -1,5 +1,6 @@
 const Doctor = (sequelize, DataTypes) => {
   const doctor = sequelize.define(
+    // Este primeiro parâmetro -- 'doctor' -- é utilizado pelo Sequelize na hora de fazer as queries pelo JavaScript.
     'doctor', {
       name: DataTypes.STRING,
       CRM: DataTypes.NUMBER,
--- register-doctors_back-end/models/specialty.js
+++ register-doctors_back-end/models/specialty.js
@@ -1,5 +1,6 @@
 const Specialty = (sequelize, DataTypes) => {
   const specialty = sequelize.define(
+    // Este primeiro parâmetro -- 'specialty' -- é utilizado pelo Sequelize na hora de fazer as queries pelo JavaScript.
     'specialty', {
       specialty: DataTypes.STRING,
     }, {

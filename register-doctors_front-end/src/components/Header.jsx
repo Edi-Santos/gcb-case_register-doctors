79bfac99807d964beade97a028c8b0ef60ccee43
Ed Santos <edsantos2822@gmail.com>
2022-04-18 15:26:37 -0300

    build: criando componente

---
+++ register-doctors_front-end/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <header>
+      <h2>Banco de Médicos</h2>
+    </header>
+  );
+}
+
+export default Header;

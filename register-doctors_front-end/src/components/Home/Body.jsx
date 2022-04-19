d7af08494a7db22a8b0adbd5bbde9d96bda4e9e0
Ed Santos <edsantos2822@gmail.com>
2022-04-19 19:32:04 -0300

    build: criando compente Body.jsx

---
+++ register-doctors_front-end/src/components/Home/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BodyCards from './cards/BodyCards';
+
+import './body.css';
+
+function Body() {
+  return (
+    <main>
+      <BodyCards />
+    </main>
+  );
+}
+
+export default Body;

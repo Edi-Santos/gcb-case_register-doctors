66aedadabfbf9d95140393e6868089c36822ee59
Ed Santos <edsantos2822@gmail.com>
2022-04-19 19:30:20 -0300

    build: adicionando compente a pagina

--- register-doctors_front-end/src/pages/Home.jsx
+++ register-doctors_front-end/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Body from '../components/Home/Body';
 
 function Home() {
   return (
     <section>
       <Header />
+      <Body />
     </section>
   );
 }

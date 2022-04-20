1da694922f00e074f982c8675af82d01e516e5f5
Ed Santos <edsantos2822@gmail.com>
2022-04-20 12:02:53 -0300

    build: adicionando compenente à página Register

--- register-doctors_front-end/src/pages/Register.jsx
+++ register-doctors_front-end/src/pages/Register.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Body from '../components/Register/Body';
 
 function Register() {
   return (
     <section>
       <Header />
+      <Body />
     </section>
   );
 }

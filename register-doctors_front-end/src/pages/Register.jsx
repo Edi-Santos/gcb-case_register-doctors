c6d6e623a0c5a55248a671c7f43d2e4ce79f8696
Ed Santos <edsantos2822@gmail.com>
2022-04-19 22:46:06 -0300

    build: criando página Register

---
+++ register-doctors_front-end/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Register() {
+  return (
+    <section>
+      <Header />
+    </section>
+  );
+}
+
+export default Register;

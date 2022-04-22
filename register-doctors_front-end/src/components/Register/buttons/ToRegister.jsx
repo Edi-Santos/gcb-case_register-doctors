b6fbb9e86a0338ef090e44ee105aed5b71ee4009
Ed Santos <edsantos2822@gmail.com>
2022-04-22 17:48:01 -0300

    build: criando função para requisição à API através do botão de cadastro

--- register-doctors_front-end/src/components/Register/buttons/ToRegister.jsx
+++ register-doctors_front-end/src/components/Register/buttons/ToRegister.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import request from '../../../services/requestToItselfAPI';
 
-function ToRegister() {
+function ToRegister({ doctorDatas }) {
+  const spec1 = 2;
+  const spec2 = 3;
+
+  // Função para requisição à API e adicionar informações ao banco de dados
+  // ===
+  const sendToBank = async () => {
+    const url = 'http://localhost:3001/doctor';
+    const method = 'POST';
+    const headers = {
+      'Content-type': 'application/json; charset=UTF-8',
+    };
+    const body = {
+      ...doctorDatas,
+      specialty_id: [spec1, spec2],
+    };
+
+    const response = await request(url, method, headers, JSON.stringify(body));
+
+    console.log(response);
+  };
+  // ===
+
   return (
-    <button type="submit">
+    <button
+      type="button"
+      onClick={ sendToBank }
+    >
       Cadastrar
     </button>
   );
 }
+
+ToRegister.propTypes = {
+  doctorDatas: PropTypes.objectOf({}).isRequired,
+};
 
 export default ToRegister;

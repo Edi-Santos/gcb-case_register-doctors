96abb2a9f296595b1dba069e607ae399e3caaa06
Ed Santos <edsantos2822@gmail.com>
2022-04-24 19:40:11 -0300

    build: adicionando botão de voltar à página principal

--- register-doctors_front-end/src/components/DoctorsList/Body.jsx
+++ register-doctors_front-end/src/components/DoctorsList/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Table from './table/Table';
+import BackToHome from '../Register/buttons/BackToHome';
 
 function Body() {
   return (
     <section>
       <Table />
+      <BackToHome />
     </section>
   );
 }
